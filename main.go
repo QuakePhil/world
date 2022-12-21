9d3728e998a8cc2c4af80da27ca1b06fc8239504
QuakePhil <phil.nyc@gmail.com>
2022-12-21 12:44:17 -0500

    organize

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 	"net/http"
 )
 
+func main() {
+	handleWebSockets("/ws") // websocket.go
+	handleLocalFile("/", "client/world.html")
+	handleLocalFile("/world.html", "client/world.html")
+	handleLocalFile("/world.css", "client/world.css")
+	handleLocalFile("/js/bouncyballs.js", "client/js/bouncyballs.js")
+	listen("localhost:8081")
+}
+
 func handleLocalFile(path, local string) {
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, local)
@@ -14,13 +23,4 @@ func handleLocalFile(path, local string) {
 func listen(address string) {
 	log.Println("Listening for http and ws on", address)
 	http.ListenAndServe(address, nil)
-}
-
-func main() {
-	handleWebSockets("/ws")
-	handleLocalFile("/", "client/world.html")
-	handleLocalFile("/world.html", "client/world.html")
-	handleLocalFile("/world.css", "client/world.css")
-	handleLocalFile("/js/bouncyballs.js", "client/js/bouncyballs.js")
-	listen("localhost:8081")
 }
