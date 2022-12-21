3b13fb4b83a04df05162dd7cb02ad3e02c7b0229
QuakePhil <phil.nyc@gmail.com>
2022-12-21 13:22:15 -0500

    style

--- client/js/bouncyballs.js
+++ client/js/bouncyballs.js
@@ -1,12 +1,12 @@
 function draw(obj) {
-    coordinates = obj.split(" ")
-    // circle
-    ctx.beginPath()
-    ctx.arc(coordinates[0], coordinates[1], 10, 0, 2 * Math.PI)
-    ctx.stroke()
-    // line
-    ctx.beginPath()
-    ctx.moveTo(coordinates[0], coordinates[1])
-    ctx.lineTo(coordinates[2], coordinates[3])
-    ctx.stroke()
+  coordinates = obj.split(' ');
+  // circle
+  ctx.beginPath();
+  ctx.arc(coordinates[0], coordinates[1], 10, 0, 2 * Math.PI);
+  ctx.stroke();
+  // line
+  ctx.beginPath();
+  ctx.moveTo(coordinates[0], coordinates[1]);
+  ctx.lineTo(coordinates[2], coordinates[3]);
+  ctx.stroke();
 }
