423afd8fe32e443c4bb8aff09a9ec3a8e572a5d1
QuakePhil <phil.nyc@gmail.com>
2022-12-21 19:59:15 -0500

    refactor

--- main.go
+++ main.go
@@ -3,15 +3,38 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	handleWebSockets("/ws") // websocket.go
-	handleLocalFile("/", "client/world.html")
-	handleLocalFile("/world.html", "client/world.html")
-	handleLocalFile("/world.css", "client/world.css")
+	handleLocal("/", "client/")
 	handleLocalFile("/js/bouncyballs.js", "client/js/bouncyballs.js")
+	ticker()
 	listen("localhost:8081")
+}
+
+func handleLocal(path, local string) {
+	waitingForFirstHtml := true
+	c, err := os.ReadDir(local)
+	check(err)
+	for _, entry := range c {
+		if !entry.IsDir() {
+			filename := entry.Name()
+			if waitingForFirstHtml && filepath.Ext(filename) == ".html" {
+				waitingForFirstHtml = false
+				handleLocalFile("/", local+filename)
+			}
+			handleLocalFile("/"+filename, local+filename)
+		}
+	}
 }
 
 func handleLocalFile(path, local string) {
