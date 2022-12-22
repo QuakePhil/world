40462620ceb8d8a7ea87d78048c17bc253fd432f
QuakePhil <phil.nyc@gmail.com>
2022-12-22 15:23:34 -0500

    cleanup

--- client/world.js
+++ client/world.js
@@ -1,19 +1,16 @@
-var latestFrame;
-
-function updateFrame(frame) {
-    if (frame.data) {
-      latestFrame = frame.data;
-    }
-}
-
-var canvas, ctx, conn;
+var canvas, ctx, conn, latestFrame;
 var fps = 60.0;
 
 window.onload = function() {
     if (window["WebSocket"]) {
         loaded()
         conn = new WebSocket("ws://" + document.location.host + "/ws")
-        conn.onmessage = updateFrame
+        conn.onmessage = function(e) {
+            if (e.data) {
+              latestFrame = e.data;
+            }
+        }
+
         setInterval(draw, 1000.0/fps);
     } else {
         console.log("Your browser does not support WebSockets!")
